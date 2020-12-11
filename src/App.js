61a9a4a231d21e501ced45a6f7aaf04033ce2439
Thomas Valvik Yttri <thoytt04@hotmail.com>
2020-12-11 01:07:15 +0100

    Added dynamic navigation with react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 //Import pages
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
 //Global style
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+//Router
+import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route exact path="/work">
+          <OurWork />
+        </Route>
+        <Route exact path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
