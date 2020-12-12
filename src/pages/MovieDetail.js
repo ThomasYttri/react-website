8aceb0b758e1137e809f9ee0c2303d5673f5d378
Thomas Valvik Yttri <thoytt04@hotmail.com>
2020-12-12 01:07:51 +0100

    Added MovieDetail site for extended project information

---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,13 @@
+//Styled
+import styled from "styled-components";
+import {useHistory} from "react-router-dom";
+
+const MovieDetail = () => {
+    return (
+        <div>
+            <h1>MovieDetails</h1>
+        </div>
+    )
+}
+
+export default MovieDetail;
