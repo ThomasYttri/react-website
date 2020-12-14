0059ac2b60e58aeeb33033a73b68ddea009b8396
Thomas Valvik Yttri <thoytt04@hotmail.com>
2020-12-15 00:00:46 +0100

    Adjusted animation speed and range

--- src/animation.js
+++ src/animation.js
@@ -1,13 +1,13 @@
 export const pageAnimation = {
     hidden: {
         opacity: 0,
-        y: 300,
+        y: 200,
     },
     show: {
         opacity: 1,
         y: 0,
         transition: {
-            duration: 1,
+            duration: 0.5,
         },
     },
     exit: {
