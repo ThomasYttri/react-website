e3d904bb3d9755ad23d47f91f5cb886bd36785d4
Thomas Valvik Yttri <thoytt04@hotmail.com>
2020-12-15 23:31:09 +0100

    Added animation to Our work page.

--- src/animation.js
+++ src/animation.js
@@ -1,11 +1,11 @@
 export const pageAnimation = {
     hidden: {
         opacity: 0,
-        y: 200,
+        //y: 200,
     },
     show: {
         opacity: 1,
-        y: 0,
+        //y: 0,
         transition: {
             duration: 0.5,
             when: "beforeChildren",
@@ -14,7 +14,7 @@ export const pageAnimation = {
     },
     exit: {
         opacity: 0,
-        y: 100,
+        //y: 100,
         transition: {
             duration: 0.25,
         },
@@ -22,7 +22,7 @@ export const pageAnimation = {
 }
 
 export const titleAnim = {
-    hidden: {y: 200},
+    hidden: {y: 300},
     show: {
         y: 0,
         transition:{duration: 0.75, ease: "easeOut"},
@@ -46,5 +46,33 @@ export const photoAnim = {
             ease: "easeOut",
             duration: 0.5,
         },
+    },
+}
+
+export const lineAnim = {
+    hidden: {width: "0%"},
+    show: {
+        width: "100%",
+        transition: {duration: 1},
+    },
+}
+
+export const slider = {
+    hidden: {x: "-130%", skew: "45deg"},
+    show: {
+        x: "100%",
+        skew: "0deg",
+        transition: {ease: "easeOut", duration: 1},
+    },
+}
+
+export const sliderContainer = {
+    hidden: {opacity: 1},
+    show: {
+        opacity: 1, 
+        transition: {
+            staggerChildren: 0.15, 
+            ease: "easeOut"
+        }
     },
 }
